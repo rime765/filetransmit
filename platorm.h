fefff6b003b3aa16d0352e4406be648fb450b804
userName <userEmail>
2026-08-10 20:40:24 +0800

    win_plate

--- platorm.h
+++ platorm.h
@@ -1,6 +1,7 @@
 #ifndef __PLATFORM_H
 #define __PLATFORM_H
-    #ifdef _WIN32
+    #include <minwindef.h>
+#ifdef _WIN32
          #ifndef _WIN32_WINNT
             #define _WIN32_WINNT 0x0601
         #endif
@@ -9,11 +10,26 @@
 
         typedef  int socket_len ;
         #define  T_SOCKEET  SOCKET
-        #define 
+        #define  T_SOCKET_INVALID   INVALID_SOCKET
+        #define  T_SOCKET_ERROR     SOCKET_ERROR 
+        #define  T_SOCKET_SUCCESS   0
 
+        #define  T_CloseSocket(socket_id)   close(socket_id)
+        #define  T_Error()                  WSAGETLASTERROR();
 
+        static inline int t_init()
+        {
+            WSADATA wsa_data;
+            return WSAStartup(/*拼版本号*/MAKEWORD(2, 2), &wsa_data);
+        }
+
+        static inline void t_exit()
+        {
+            WSACleanup();
+        }
 
     #else
+        
 
     #endif
 
